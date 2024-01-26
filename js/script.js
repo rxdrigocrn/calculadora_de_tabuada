279f013b13a57ac02e273bd766d79f1851ad9370
rodrigocrn <rodrigocirinocl@gmail.com>
2024-01-25 22:18:47 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicador");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+
+
+  
+  for (i = 1; i < multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const template = `
+        <div class = "row"> 
+        <div class = "operation">${number} x ${i} =</div>
+        <div class = "result">${result}</div>
+        </div>`;
+
+
+
+        
+    // colocar no html 
+
+    const parser = new DOMParser()
+    const htmltemplate = parser.parseFromString(template, "text/html")
+    const row = htmltemplate.querySelector(".row")
+    multiplicationTable.appendChild(row)
+
+
+  }
+
+  multiplicationTitle.innerHTML = number 
+};
+
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = +numberInput.value;
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  createTable(multiplicationNumber, multiplicatorNumber);
+});
